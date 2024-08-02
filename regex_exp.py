2a57674b89562435c4bd99cec318e4f2d9bf9f93
kareemrogers-coder <kareemrogers27@gmail.com>
2024-08-02 01:21:15 -0400

    Module 3 lesson4 commit

---
+++ regex_exp.py
@@ -0,0 +1,26 @@
+# 1. Python Regular Expressions Mastery
+
+
+
+# Task 1: Name Verification
+
+
+
+# Problem Statement:  Write a function that takes in a list of names, and verifies that the names are valid names using a regex pattern and match(), and prints the name if it is valid, "Invalid name" if the name is not.
+
+# Use the following list as an argument to test your function.
+
+import re
+
+names = ["Abraham Lincoln", "Andrew P Garfield", "peter pan", "Connor Milliken", "Jordan Alexander Williams", "Madonna", "programming is cool"]
+
+def valid_names():
+    for name in names:
+    #print(name)
+        val_name = re.match(r"\D+[A-Z]\D+", name)
+        if val_name:
+            print(name)
+        else:
+            print("Invalid Name")
+valid_names()
+
